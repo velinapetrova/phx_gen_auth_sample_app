9aabc805b958209ab6eac630b61064fb48ce1723
Velina Petrova <velinarp@gmail.com>
2021-01-28 22:41:12 +0800

    add phx_gen_auth dependancy
    Signed-off-by: Velina Petrova <velinarp@gmail.com>

--- mix.exs
+++ mix.exs
@@ -45,6 +45,7 @@ defmodule PhxGenAuthSampleApp.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
+      {:phx_gen_auth, "~> 0.6", only: [:dev], runtime: false},
     ]
   end
 
